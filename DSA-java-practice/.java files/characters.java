11bdac9f39bf38a245d30e88f110c40fd4e0e190
Akshita-star <panwarakshita55@gmail.com>
2025-07-21 13:08:39 +0530

    Create characters.java

---
+++ DSA-java-practice/.java files/characters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class characters {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String ch=sc.next();//reads string
+        char c=sc.next().charAt(0);//reads chrcter
+        System.out.println(ch);
+        System.out.println(c);
+        System.out.println((int)c);
+        
+    }
+    
+}
