01c1e8b3ba230480a3a5bfe7d93cd2c0d8b4cdeb
Akshita-star <panwarakshita55@gmail.com>
2025-07-21 12:36:05 +0530

    .

---
+++ DSA-java-practice/.java files/modulusoperator.java
@@ -0,0 +1,8 @@
+public class modulusoperator {
+    public static void main(String[] args) {
+        System.out.println(5%2);//remainder
+        System.out.println(2%5);//a<b
+        System.out.println(5%-2);//same as a%b
+        System.out.println(-5%2);//-(a%b)
+    }
+}
