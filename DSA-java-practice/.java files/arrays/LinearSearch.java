b6559b03354c09b8bcd4d40b75e991d1e01e816d
Akshita-star <panwarakshita55@gmail.com>
2025-08-12 09:30:42 +0530

    .

---
+++ DSA-java-practice/.java files/arrays/LinearSearch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class LinearSearch{
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int x=sc.nextInt();
+       int n=sc.nextInt();
+       int []arr=new int[n];
+       for(int i=0;i<n;i++){
+          arr[i]=sc.nextInt();
+       }
+       int flag=0;
+        for(int i=0;i<n;i++){
+           if(arr[i]==x){
+              flag=1;
+              break;
+            }
+        }
+        if(flag==1){
+            System.out.println("found");
+        }
+        else{
+            System.out.println("not found");
+        }
+       
+    }
+
+}
--- DSA-java-practice/.java files/basics and conditionals/ModulusOperator.java
+++ DSA-java-practice/.java files/basics and conditionals/ModulusOperator.java
@@ -1,4 +1,4 @@
-public class modulusoperator {
+public class ModulusOperator {
     public static void main(String[] args) {
         System.out.println(5%2);//remainder
         System.out.println(2%5);//a<b
